70e8b1301b8663cc31e004edf295be7922e01953
javier938a <carlos8a_93@yahoo.com>
2022-09-28 17:34:47 -0600

    Agregando validaciones

--- recursos.py
+++ recursos.py
@@ -25,14 +25,15 @@ class MyLogger(object):
 class HiloDescargador(threading.Thread):
     list_progreso_descarga = []
 
-    def __init__(self, nombre_hilo, url, ruta,item, contendor_descarga,daemon=None, key_des=None):
+    def __init__(self, nombre_hilo, url, ruta, ydl_opts,list_progreso_descarga,item, contendor_descarga,daemon=None, key_des=None):
         threading.Thread.__init__(self, name=nombre_hilo, target=HiloDescargador.run, daemon=daemon)
+        self.ydl_opts=ydl_opts
         self.url=url
         self.ruta=ruta
         self.descargas={}
         self.contenedor_descarga=contendor_descarga
 
-        self.list_progreso_descarga.append({nombre_hilo: item})  # agrego un diccionario de progreos a una lista global para ir viendo los prograsos independiente
+        self.list_progreso_descarga=list_progreso_descarga  # agrego un diccionario de progreos a una lista global para ir viendo los prograsos independiente
 
 
         self.contenedor_descarga.add_widget(item[0])
@@ -60,9 +61,10 @@ class HiloDescargador(threading.Thread):
                     lista_nombre_media_con_extencion=nombre_media_con_extension.split('.')
                     nombre_media=lista_nombre_media_con_extencion[0]
 
-                    progreso[self.name][0].ids.lbl_porcentaje.text=str(porcentaje)+"% velocidad "+str(d['_speed_str'])
+                    progreso[self.name][0].ids.lbl_porcentaje.text=str(porcentaje)+"% velocidad "+str(d['_speed_str'])+" formato de salida: "+progreso["formato"]
                     progreso[self.name][0].ids.lbl_ubicacion.text="Descargando: [size=11]"+nombre_media+"[/size]"
                     progreso[self.name][1].value=porcentaje
+
                     #print(d)
 
 
@@ -70,28 +72,18 @@ class HiloDescargador(threading.Thread):
         print("Porcentaje: "+str(porcentaje))
 
         if d['status'] == 'finished':
-            print('descarga finalizada, convertido')
+            for descarga in self.list_progreso_descarga:
+                if self.name in descarga:
+                    descarga.pop(self.name)
+            print('descarga finalizada, convertido, y eliminado de la lista')
 
 
     def run(self):
         url_descarga = self.url
-        ydl_opts = {
-            #'format': 'bestaudio/best,'
-            'format':'bestvideo+bestaudio',
-            'nocheckcertificate': True,
-            #'cachedir':False,
-            'outtmpl': self.ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
-            'postprocessors': [{
-                #'key': 'FFmpegExtractAudio',
-                'key':'FFmpegVideoConvertor',
-                'preferedformat': 'mp4',
-                #'preferredquality': '192',
-            }],
-            'logger': MyLogger(),
-            'progress_hooks': [self.progreso_descarga]
-        }
+        self.ydl_opts['logger']=MyLogger()#añadiendole el logger
+        self.ydl_opts['progress_hooks']=[self.progreso_descarga]#llamando a la funcion encargada del progreso de cada MDProgressBar
 
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        with youtube_dl.YoutubeDL(self.ydl_opts) as ydl:
             ydl.download([url_descarga])
 
 
--- start.py
+++ start.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import youtube_dl
 import threading
 from kivy.properties import ObjectProperty
+from kivy.metrics import dp
 from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
@@ -13,9 +14,12 @@ from kivymd.uix.filemanager import MDFileManager
 from kivymd.icon_definitions import md_icons
 import clipboard as cop #biblioteca para copiar y pegar
 from recursos import  ContenedorRigidoDescargas
-from kivymd.uix.list import OneLineListItem
+from kivymd.uix.list import OneLineListItem, OneLineIconListItem
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.toast import toast
+from kivymd.uix.dropdownitem import MDDropDownItem
 from recursos import HiloDescargador
-from kivy.properties import ObjectProperty, ListProperty
+from kivy.properties import ObjectProperty, ListProperty, StringProperty,BooleanProperty
 from recursos import CopyPasteUrl
 
 
@@ -43,6 +47,8 @@ colors={
 class Tab(MDBoxLayout, MDTabsBase):
     icon = ObjectProperty()
 
+class IconListItem(OneLineIconListItem):
+    pass
 
 class MyLogger(object):
     def debug(self, msg):
@@ -57,6 +63,10 @@ class VentanaInicial(MDBoxLayout):
     icons=list(md_icons.keys())[15:30]
     hilodescarga=ObjectProperty(None)
     lista_de_hilos=[]
+    formato_convertir=StringProperty("")
+    list_progreso_descarga=ListProperty([])
+
+
 
     def __init__(self):
         super(VentanaInicial, self).__init__()
@@ -68,13 +78,43 @@ class VentanaInicial(MDBoxLayout):
             preview=True
         )
         print(self.ids)
+        lista_formatos=[
+            'mp3',
+            'mp4',
+            'avi',
+            'flv',
+            'mkv',
+            'ogg',
+            'webm'
+        ]
+        menu_item_format=[
+            {
+                "viewclass": "IconListItem",
+                'icon':"git",
+                "text": f"{i}",
+                "height":dp(56),
+                "on_release": lambda x=f"{i}": self.seleccion_del_menu(x),
+            } for i in lista_formatos
+        ]
+        self.menu_format=MDDropdownMenu(
+            caller=self.ids.btn_formato_convertir,
+            items=menu_item_format,
+            position="center",
+            width_mult=4
+        )
         self.ids.btn_descargar.bind(on_release=self.add_new_descarga)
         self.ids.btn_listar_directorios.bind(on_release=self.on_listar_directorios)
+
         '''
         for name_tab in self.icons:
             tab=Tab(title="This is "+name_tab, icon=name_tab)
             self.ids.tabs.add_widget(tab)   
         '''
+    def seleccion_del_menu(self, text_item):
+        self.formato_convertir=text_item
+        self.ids.lbl_muestra_format.text="Convertir a "+text_item
+        print(text_item)
+        self.menu_format.on_dismiss()
     def seleccionar_ruta(self, path):
         self.ids.txt_ruta.text=path
         self.salir_manager()
@@ -157,12 +197,136 @@ class VentanaInicial(MDBoxLayout):
         ruta=self.ids.txt_ruta.text
 
         key_des=0
-        descarga_item_list=ItemDescarga()
-        item=[descarga_item_list, descarga_item_list.ids.progres_download]
+
+        #estas variables se modificaran segun el tipo de archivo que se desee
+        ydl_opts={}
+        if url!="" and ruta!="":
+            if self.formato_convertir=="mp3":
+                ydl_opts = {
+                    'format': 'bestaudio/best,',
+                    'nocheckcertificate': True,
+                    # 'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                        'preferredquality': '192',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            elif self.formato_convertir=="mp4":
+                ydl_opts = {
+                    'format':'bestvideo+bestaudio',
+                    'nocheckcertificate': True,
+                    #'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key':'FFmpegVideoConvertor',
+                        'preferedformat': 'mp4',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            elif self.formato_convertir=="avi":
+                ydl_opts = {
+                    'format':'bestvideo+bestaudio',
+                    'nocheckcertificate': True,
+                    #'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key':'FFmpegVideoConvertor',
+                        'preferedformat': 'avi',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            elif self.formato_convertir=='mkv':
+                ydl_opts = {
+                    'format':'bestvideo+bestaudio',
+                    'nocheckcertificate': True,
+                    #'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key':'FFmpegVideoConvertor',
+                        'preferedformat': 'mkv',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            elif self.formato_convertir=="flv":
+                ydl_opts = {
+                    'format':'bestvideo+bestaudio',
+                    'nocheckcertificate': True,
+                    #'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key':'FFmpegVideoConvertor',
+                        'preferedformat': 'flv',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            elif self.formato_convertir=="ogg":
+                ydl_opts = {
+                    'format':'bestvideo+bestaudio',
+                    'nocheckcertificate': True,
+                    #'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key':'FFmpegVideoConvertor',
+                        'preferedformat': 'ogg',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            elif self.formato_convertir=="webm":
+                ydl_opts = {
+                    'format':'bestvideo+bestaudio',
+                    'nocheckcertificate': True,
+                    #'cachedir':False,
+                    'outtmpl': ruta + '/%(extractor_key)s/%(extractor)s-%(id)s-%(title)s.%(ext)s',
+                    'postprocessors': [{
+                        'key':'FFmpegVideoConvertor',
+                        'preferedformat': 'webm',
+                    }],
+                    'logger': None,
+                    'progress_hooks': None
+                }
+            existe_descarga=False#Variable que va servir para verificar si existe ya una descarga a nombre de la ruta indicada
+            if len(self.list_progreso_descarga)>0:#primero se verifica si en la lista ya hay videos descargandose para verificar si hay videos del mismo descargandose
+                for dic_url_descarga in self.list_progreso_descarga:
+                    if url in dic_url_descarga:#validando que la url ya exista dentro de la lista de descargas
+                        existe_descarga=True#si la url existe en la lista significa que existe
 
 
-        hilodescarga=HiloDescargador(url,url, ruta, item,contenedor_descarga, daemon=True, key_des=key_des)
-        hilodescarga.start()
+                if existe_descarga==True:#si existe cambia a True significa que no hay que agregar ninguna otra descarga
+                    toast("Esta video ya se esta descargando porfavor, si desea descargarlo en otro formato cierre y ejecute esta app nuevamente",
+                          duration=3.5)
+                else:#si no existe entonces se agrega el nuevo hilo para descargar el video
+                    descarga_item_list = ItemDescarga()
+                    item = [descarga_item_list, descarga_item_list.ids.progres_download]
+                    toast("Video descargando", duration=2.5)
+                    self.list_progreso_descarga.append({url: item, 'formato': self.formato_convertir})
+                    hilodescarga = HiloDescargador(url, url, ruta, ydl_opts, self.list_progreso_descarga, item,
+                                                   contenedor_descarga, daemon=True, key_des=key_des)
+                    hilodescarga.start()
+
+
+            else:#de lo contrario si es igual a cero es porque no hay ningun elemento descargandose
+                descarga_item_list = ItemDescarga()
+                item = [descarga_item_list, descarga_item_list.ids.progres_download]
+                toast("Video descargando", duration=2.5)
+                self.list_progreso_descarga.append({url: item, 'formato': self.formato_convertir})
+                hilodescarga = HiloDescargador(url, url, ruta, ydl_opts, self.list_progreso_descarga, item,
+                                               contenedor_descarga, daemon=True, key_des=key_des)
+                hilodescarga.start()
+        else:
+            toast("Debe de ingresar la URL del video puede ser de YouTube, Facebook o Vimeo ")
+
+
+
+
 
 
 
@@ -185,10 +349,12 @@ class VentanaApp(MDApp):
 
 
     def build(self):
+        ventana=VentanaInicial()
+        ventana.menu_format.background_color=self.theme_cls.primary_light
         self.theme_cls.colors=colors
         self.theme_cls.primary_palette="Red"
         self.theme_cls.accent_palette="Teal"
-        return VentanaInicial()
+        return ventana
 
 
 if __name__=="__main__":
